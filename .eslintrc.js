878b798eddd74763969cee1a04efaa45f376f775
Che' J. Holloway <chejholloway@outllok.com>
2023-08-24 14:01:07 -0700

    refactor(global): :art: gets the app to function with requirements

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,74 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
-    es6: true,
-    jest: true,
-    node: true
-  },
-  parserOptions: {
-    root: true,
-    ecmaVersion: 'latest',
-    project: ['./tsconfig.json'],
-    sourceType: 'module',
-    parser: '@typescript-eslint/parser'
+    es2021: true
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb',
-    'airbnb/hooks',
-    'airbnb-typescript',
-    'prettier'
+    'plugin:@typescript-eslint/recommended'
   ],
-  overrides: [
-    {
-      extends: ['plugin:@typescript-eslint/disable-type-checked'],
-      files: ['./**/*.js']
-    }
-  ],
-  plugins: ['react', 'unused-imports', 'tailwindcss'],
-  rules: {
-    'no-console': 'off',
-    'import/prefer-default-export': 'off',
-    '@typescript-eslint/camelcase': ['off'],
-    'camelcase': ['off'],
-    'react/jsx-props-no-spreading': ['off'],
-    'jsx-a11y/no-static-element-interactions': 'off',
-    'react/prop-types': ['off'],
-    'jsx-a11y/href-no-hash': ['off'],
-    'jsx-a11y/click-events-have-key-events': ['off'],
-    'import/no-unresolved': ['off'],
-    'import/extensions': 0,
-    'no-use-before-define': 'off',
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [
-      'warn',
-      {
-        extensions: ['.js', '.jsx', '.tsx', '.ts']
-      }
-    ],
-    'max-len': [
-      'warn',
-      {
-        code: 100,
-        tabWidth: 2,
-        comments: 80,
-        ignoreComments: false,
-        ignoreTrailingComments: true,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true
-      }
-    ],
-    'react/function-component-definition': [
-      'error',
-      {
-        namedComponents: ['function-declaration', 'arrow-function'],
-        unnamedComponents: 'arrow-function'
-      }
-    ],
-    'arrow-body-style': ['error', 'as-needed']
-  }
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {}
 };
