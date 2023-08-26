d41485c536ea6059b07c4d98e9c2559e48c342c8
Che' J. Holloway <chejholloway@outllok.com>
2023-08-25 22:28:14 -0700

    fix(global): :ambulance: app wasn't building
    have to add tests back

--- react-docgen.config.js
+++
@@ -1,5 +0,0 @@
-// react-docgen.config.js
-module.exports = {
-  entry: './src/components', // Path to your components directory
-  output: './docs/components', // Path to the output directory for generated docs
-};
--- src/components/followers-list/FollowersList.tsx
+++ src/components/followers-list/FollowersList.tsx
@@ -1,70 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Avatar, Typography } from '@material-tailwind/react';
-
+import { Avatar } from '@material-tailwind/react';
 import Follower from '../../types/Follower';
+import List from '../list/List';
 
-/**
- * Renders a list of followers with their avatars.
- *
- * This component takes an array of `Follower` objects as a prop and renders
- * a list of avatars with links to their profiles.
- *
- * @component
- * @example
- * ```tsx
- * const followers: Follower[] = [
- *   {
- *     id: 1,
- *     login: "exampleUser",
- *     avatar_url: "https://example.com/avatar.jpg"
- *   },
- *   ...more followers
- * ];
- *
- * <FollowersList followers={followers} />
- * ```
- * @defaultprops
- * @param {object} props - The component props.
- * @param {Follower[]} props.followers - An array of `Follower` objects representing followers.
- */
 interface FollowersListProps {
   followers: Follower[] | undefined;
 }
 
-/**
- * Renders a list of followers with their avatars.
- * @defaultprops
- * @param {FollowersListProps} props - The component props.
- * @returns {JSX.Element} The rendered component.
- */
-
 const FollowersList: React.FC<FollowersListProps> = ({ followers }) => {
-  return (
-    <div className='text-gray-700'>
-      <h3 className='text-center'>
-        <Typography className='font-normal'>Followers</Typography>
-      </h3>
-      <div className='flex flex-wrap space-x-4'>
-        {followers ? (
-          followers.map((follower) => (
-            <div key={follower.id}>
-              <Link to={'/'}>
-                <Avatar
-                  variant='circular'
-                  title={`${follower?.login}'s Avatar`}
-                  alt={follower?.login}
-                  src={follower?.avatar_url}
-                />
-              </Link>
-            </div>
-          ))
-        ) : (
-          <p>No followers available.</p>
-        )}
-      </div>
-    </div>
+  const renderFollower = (follower: Follower) => (
+    <Avatar
+      variant='circular'
+      title={`${follower?.login}'s Avatar`}
+      alt={follower?.login}
+      src={follower?.avatar_url}
+    />
   );
+
+  return <List data={followers} renderItem={renderFollower} developer={null} title='Followers' />;
 };
 
 export default FollowersList;
