1ecdbee97b95315da2c6206e2d2f3a0988369d42
Che' J. Holloway <chejholloway@outllok.com>
2023-08-25 21:15:49 -0700

    fix(global): :white_check_mark: fixing tests, to make sure repo runs
    cleans up develop branch

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { createRoot } from 'react-dom'; // Update import here
+import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@material-tailwind/react';
-import configureMockStore from 'redux-mock-store';
+
 import App from './app/App';
 import store from './store/store';
 import './index.css';
 
-const mockStore = configureMockStore();
+const app = document.getElementById('root') as HTMLElement;
+const root = createRoot(app);
 
-describe('App', () => {
-  it('renders without crashing', () => {
-    const app = document.createElement('div');
-    app.id = 'root';
-    document.body.appendChild(app);
-
-    const root = createRoot(app);
-
-    const initialState = {};
-    const store = mockStore(initialState);
-
-    root.render(
-      <Provider store={store}>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </Provider>
-    );
-  });
-});
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
--- src/types/Repo.test.ts
+++ src/types/Repo.test.ts
@@ -6,24 +6,24 @@ import Owner from './Owner'; // Import the Owner interface
 describe('Repo interface', () => {
   it('should have the required properties', () => {
     const owner: Owner = {
-      login: 'example',
-      id: 123,
-      node_id: 'abc456',
       avatar_url: 'https://example.com/owner-avatar.png',
-      gravatar_id: 'gravatar123',
-      url: 'https://api.github.com/users/example',
-      html_url: 'https://github.com/example',
+      events_url: 'https://api.github.com/users/example/events{/privacy}',
       followers_url: 'https://api.github.com/users/example/followers',
       following_url: 'https://api.github.com/users/example/following{/other_user}',
       gists_url: 'https://api.github.com/users/example/gists{/gist_id}',
+      gravatar_id: 'gravatar123',
+      html_url: 'https://github.com/example',
+      id: 123,
+      login: 'example',
+      node_id: 'abc456',
+      organizations_url: 'https://api.github.com/users/example/orgs',
+      received_events_url: 'https://api.github.com/users/example/received_events',
+      repos_url: 'https://api.github.com/users/example/repos',
+      site_admin: false,
       starred_url: 'https://api.github.com/users/example/starred{/owner}{/repo}',
       subscriptions_url: 'https://api.github.com/users/example/subscriptions',
-      organizations_url: 'https://api.github.com/users/example/orgs',
-      repos_url: 'https://api.github.com/users/example/repos',
-      events_url: 'https://api.github.com/users/example/events{/privacy}',
-      received_events_url: 'https://api.github.com/users/example/received_events',
       type: 'User',
-      site_admin: false
+      url: 'https://api.github.com/users/example',
     };
 
     const repo: Repo = {
@@ -31,8 +31,7 @@ describe('Repo interface', () => {
       node_id: 'def789',
       name: 'example-repo',
       full_name: 'example/example-repo',
-      private: false,
-      owner: owner
+      private: false
     };
 
     expect(repo.id).toBeDefined();
@@ -40,6 +39,5 @@ describe('Repo interface', () => {
     expect(repo.name).toBeDefined();
     expect(repo.full_name).toBeDefined();
     expect(repo.private).toBeDefined();
-    expect(repo.owner).toBeDefined();
   });
 });
