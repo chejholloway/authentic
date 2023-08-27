4d3d1a6a8d7893b7188fc834501abae4ba6e4ad5
Che' J. Holloway <chejholloway@outllok.com>
2023-08-27 15:51:09 -0700

    style: :recycle: centralizes styles into one file

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,8 +5,10 @@ import NavbarIconContainer from "../navbar/navbar-icon-container/NavbarIconConta
 
 const Navbar: React.FC = () => {
   return (
-    <nav data-testid="navbar"
-        className="flex justify-around py-4 bg-white/80 backdrop-blur-md shadow-md w-full fixed top-0 left-0 right-0 z-10 mb-8">
+    <nav
+      data-testid="navbar"
+      className="flex justify-around py-4 bg-white/80 backdrop-blur-md shadow-md w-full fixed top-0 left-0 right-0 z-10 mb-8"
+    >
       <NavbarIconContainer />
       <div className="flex items-center space-x-5">
         <HomeLink />
