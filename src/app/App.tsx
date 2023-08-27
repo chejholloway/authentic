326cb382544f38f2af71630f235df32a5d1ce4f5
Che' J. Holloway <chejholloway@outllok.com>
2023-08-26 21:51:35 -0700

    feat: :sparkles: adds 404 page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import ErrorPage from '../pages/error/ErrorPage'
 import HomePage from "../pages/home/HomePage";
 import DeveloperDetailPage from "../pages/detail/DeveloperDetailPage";
 
@@ -8,6 +9,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+        <Route path="*" element={<ErrorPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/developer/:username" element={<DeveloperDetailPage />} />
       </Routes>
